567f3427663c2babbed8ab0f71ce1ac11d156ce7
389093982 <389093982@qq.com>
2018-10-28 20:07:43 +0800

    markdown样式优化

--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/IBlog/BlogAdd.vue
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/IBlog/BlogAdd.vue
@@ -15,7 +15,7 @@
         </Select>
       </FormItem>
       <FormItem label="文章内容" prop="content">
-        <mavon-editor v-model="formValidate.content" :ishljs = "true"/>
+        <mavon-editor v-model="formValidate.content" :toolbars="toolbars" :ishljs = "true" style="z-index: 1;"/>
       </FormItem>
       <FormItem>
         <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
@@ -33,6 +33,27 @@
     name: "BlogAdd",
     data () {
       return {
+        toolbars: {
+          bold: true, // 粗体
+          italic: true, // 斜体
+          header: true, // 标题
+          underline: true, // 下划线
+          // mark: true, // 标记
+          superscript: true, // 上角标
+          quote: true, // 引用
+          ol: true, // 有序列表
+          link: true, // 链接
+          imagelink: true, // 图片链接
+          help: true, // 帮助
+          code: true, // code
+          subfield: true, // 是否需要分栏
+          fullscreen: true, // 全屏编辑
+          readmodel: true, // 沉浸式阅读
+          undo: true, // 上一步
+          trash: true, // 清空
+          save: true, // 保存（触发events中的save事件）
+          navigation: true // 导航目录
+        },
         // 我的所有文章分类
         mycatalogs:[],
         formValidate: {
