048f4f0129f7ef1aaad9a34b8e2efb50133c54f9
389093982 <389093982@qq.com>
2018-10-30 00:03:53 +0800

    代码优化

--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/EasyShare/HotShareLinkItem.vue
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/EasyShare/HotShareLinkItem.vue
@@ -1,12 +1,25 @@
 <template>
-  <div style="margin-left: 150px;margin-right: 150px;">
+  <div style="margin-left: 50px;margin-right: 50px;">
     <Row :gutter="10">
-      <Col span="6" style="text-align: center;" v-for="share_type in getCurrentPage">
-        <div class="item" style="margin-left:10px; margin-right:10px; height: 100px;">{{share_type}}</div>
+      <Col span="6" v-for="share_type in getCurrentPage">
+        <a href="javascript:;" style="color: #999;">
+          <div class="item" style="margin:10px; padding:10px; height: 100px;">
+            <Row>
+              <Col span="6">
+                <img src="https://gocn.vip/uploads/nav_menu/12.jpg" alt="每日新闻">
+                <!--<img :src="share_type.img" :alt="share_type.name"/>-->
+              </Col>
+              <Col span="18">
+                <p class="share_type_name">{{share_type.name}}</p>
+                <p style="font-size: 12px;">{{share_type.detail}}</p>
+              </Col>
+            </Row>
+          </div>
+        </a>
       </Col>
     </Row>
     <Row>
-      <a href="javascript:;" style="float: right;margin: 10px 30px 10px 0;font-size: 12px;">换一批</a>
+      <a href="javascript:;" style="float: right;margin: 0px 30px 10px 0;font-size: 12px;">换一批</a>
     </Row>
   </div>
 </template>
@@ -36,9 +49,17 @@
 </script>
 
 <style scoped>
+  *{
+    font-family: 'Helvetica Neue', 'STHeiti', '微软雅黑', 'Microsoft YaHei', Helvetica,Arial,sans-serif;
+  }
   .item:hover{
     background: #ebebeb;
     border-radius: 10px;
-    font-family: 'Helvetica Neue', 'STHeiti', '微软雅黑', 'Microsoft YaHei', Helvetica,Arial,sans-serif;
+  }
+  .share_type_name{
+    color:green;
+  }
+  .share_type_name:hover{
+    color:red;
   }
 </style>
--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/GlobalData/index.js
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/GlobalData/index.js
@@ -1,6 +1,37 @@
 
 // 热门分享类型
-const hot_share_type=["Java","Python","Golang","Java","Python","Golang","Java","Python","Golang"];
+const hot_share_type=[
+  {
+    "name":"Java社区",
+    "img":"xxxx",
+    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+  },
+  {
+    "name":"Java社区",
+    "img":"xxxx",
+    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+  },
+  {
+    "name":"Java社区",
+    "img":"xxxx",
+    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+  },
+  {
+    "name":"Java社区",
+    "img":"xxxx",
+    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+  },
+  {
+    "name":"Java社区",
+    "img":"xxxx",
+    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+  },
+  {
+    "name":"Java社区",
+    "img":"xxxx",
+    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+  },
+];
 
 export default {
   hot_share_type,
