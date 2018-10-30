354fee5c12af03ff522b881063394325a19a465e
389093982 <389093982@qq.com>
2018-10-30 23:58:08 +0800

    代码优化

--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/EasyShare/HotShareLinkItem.vue
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/EasyShare/HotShareLinkItem.vue
@@ -1,15 +1,17 @@
 <template>
-  <div style="margin-left: 50px;margin-right: 50px;">
-    <Row :gutter="10">
-      <Col span="6" v-for="share_type in getCurrentPage">
+  <div style="margin-left: 50px;margin-right: 50px;margin-bottom: 20px;">
+    <Row>
+      <Col span="2" style="text-align: center;padding-top: 25px;">
+        <a href="javascript:;" @click="previous" v-show="showPrevious"><img src="/static/images/arrow_left.png"/></a>
+      </Col>
+      <Col span="5" v-for="share_type in getCurrentPage">
         <a href="javascript:;" style="color: #999;">
-          <div class="item" style="margin:10px; padding:10px; height: 100px;">
+          <div class="item" style="padding:10px; height: 100px;">
             <Row>
               <Col span="6">
-                <img src="https://gocn.vip/uploads/nav_menu/12.jpg" alt="每日新闻">
-                <!--<img :src="share_type.img" :alt="share_type.name"/>-->
+                <img :src="share_type.img" :alt="share_type.name"/>
               </Col>
-              <Col span="18">
+              <Col span="18" style="padding-left: 5px;">
                 <p class="share_type_name">{{share_type.name}}</p>
                 <p style="font-size: 12px;">{{share_type.detail}}</p>
               </Col>
@@ -17,9 +19,9 @@
           </div>
         </a>
       </Col>
-    </Row>
-    <Row>
-      <a href="javascript:;" style="float: right;margin: 0px 30px 10px 0;font-size: 12px;">换一批</a>
+      <Col span="2" style="text-align: center;padding-top: 25px;">
+        <a href="javascript:;" @click="next"  v-show="showNext"><img src="/static/images/arrow_right.png"/></a>
+      </Col>
     </Row>
   </div>
 </template>
@@ -37,8 +39,27 @@
       getCurrentPage:function(){
         return this.pagination(this.currentPageNo,4,this.hot_share_type);
       },
+      showPrevious:function () {
+        const total_page = Math.ceil(this.hot_share_type.length / 4);
+        return this.currentPageNo > 1;
+      },
+      showNext:function () {
+        const total_page = Math.ceil(this.hot_share_type.length / 4);
+        return this.currentPageNo < total_page;
+      }
     },
     methods:{
+      previous: function(){
+        if(this.currentPageNo > 1){
+          this.currentPageNo = this.currentPageNo - 1;
+        }
+      },
+      next: function(){
+        const total_page = Math.ceil(this.hot_share_type.length / 4);
+        if(this.currentPageNo < total_page){
+          this.currentPageNo = this.currentPageNo + 1;
+        }
+      },
       // 对数组进行也分
       pagination: function (pageNo, pageSize, array) {
         let offset = (pageNo - 1) * pageSize;
--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/GlobalData/index.js
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/GlobalData/index.js
@@ -3,33 +3,63 @@
 const hot_share_type=[
   {
     "name":"Java社区",
-    "img":"xxxx",
-    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+    "img":"/static/images/java_50_50.jpg",
+    "detail":"简单、直观、强悍，一次编译，到处运行。",
+  },
+  {
+    "name":"Python社区",
+    "img":"/static/images/python_50_50.jpg",
+    "detail":"简单、易学、免费/开源，丰富而且强大的库。",
+  },
+  {
+    "name":"Golang社区",
+    "img":"/static/images/golang_50_50.jpg",
+    "detail":"执行速度和开发效率出色，语言级别上支持协程并发。",
+  },
+  {
+    "name":"招聘空间",
+    "img":"/static/images/job_50_50.jpg",
+    "detail":"简洁、直观、强悍的前端框架，让web开发更迅速简单。",
   },
   {
     "name":"Java社区",
-    "img":"xxxx",
-    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+    "img":"/static/images/golang_50_50.jpg",
+    "detail":"简洁、直观、强悍的前端框架，让web开发更迅速简单。",
   },
   {
     "name":"Java社区",
-    "img":"xxxx",
-    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+    "img":"/static/images/golang_50_50.jpg",
+    "detail":"简洁、直观、强悍的前端框架，让web开发更迅速简单。",
   },
   {
     "name":"Java社区",
-    "img":"xxxx",
-    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+    "img":"/static/images/golang_50_50.jpg",
+    "detail":"简洁、直观、强悍的前端框架，让web开发更迅速简单。",
   },
   {
     "name":"Java社区",
-    "img":"xxxx",
-    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+    "img":"/static/images/golang_50_50.jpg",
+    "detail":"简洁、直观、强悍的前端框架，让web开发更迅速简单。",
   },
   {
     "name":"Java社区",
-    "img":"xxxx",
-    "detail":"简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。",
+    "img":"/static/images/java_50_50.jpg",
+    "detail":"简单、直观、强悍，一次编译，到处运行。",
+  },
+  {
+    "name":"Python社区",
+    "img":"/static/images/python_50_50.jpg",
+    "detail":"简单、易学、免费/开源，丰富而且强大的库。",
+  },
+  {
+    "name":"Golang社区",
+    "img":"/static/images/golang_50_50.jpg",
+    "detail":"执行速度和开发效率出色，语言级别上支持协程并发。",
+  },
+  {
+    "name":"招聘空间",
+    "img":"/static/images/job_50_50.jpg",
+    "detail":"简洁、直观、强悍的前端框架，让web开发更迅速简单。",
   },
 ];
 
