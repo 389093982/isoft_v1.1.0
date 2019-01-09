9e9407f15c548408a43046b0a789d70487d7d4e3
389093982 <389093982@qq.com>
2019-01-09 21:27:03 +0800

    代码优化

--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/Common/link/IBeautifulTabLink.vue
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/Common/link/IBeautifulTabLink.vue
@@ -1,298 +1,118 @@
 <template>
   <div class="container">
     <div class="price">
-      <h2>产品定价</h2>
+      <h2>{{pData.title}}</h2>
       <div class="category">
         <ul>
-          <li class="active">热门</li>
-          <li>计算</li>
-          <li>存储</li>
-          <li>视频服务</li>
-          <li>数据库</li>
-          <li>网络</li>
-          <li>CDN与加速</li>
-          <li>互联网中间件</li>
-          <li>域名服务</li>
-          <li>游戏服务</li>
-          <li>通信服务</li>
-          <li>安全</li>
-          <li>人工智能</li>
-          <li>语音服务</li>
-          <li>移动服务</li>
-          <li>数据处理</li>
-          <li>应用服务</li>
+          <li :class="index == 0 ? 'active' : ''" v-for="(itemData, index) in pData.pItemData">{{itemData.itemTitle}}</li>
         </ul>
-        <a href="javascript:;" class="prev"><span></span></a>
-        <a href="javascript:;" class="next"><span></span></a>
       </div>
-      <div class="cont active">
+      <div class="cont" :class="index == 0 ? 'active' : ''" v-for="(itemData, index) in pData.pItemData">
         <ul>
-          <li>
-            <a href="#">
+          <li v-for="item in itemData.items">
+            <a :href="item.item_link">
               <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云数据库 MySQL</h3>
-                <p class="desc">一体化多维度监控，高效管理海量数据库</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">对象存储 COS</h3>
-                <p class="desc">可靠、安全、易用的可扩展文件存储</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">内容分发网络 CDN</h3>
-                <p class="desc">多借点全网覆盖、安全稳定的内容加速服务</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">BGP高防</h3>
-                <p class="desc">高达300G的防护服务和多达21线的BGP线路抵御DDoS攻击</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云解析</h3>
-                <p class="desc">向全网域名提供稳定、安全、快速的智能解析服务</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">万象优图</h3>
-                <p class="desc">高效图片处理、全面的图片鉴定和识别服务</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">直播</h3>
-                <p class="desc">专业稳定快速的直播接入和分发服务</p>
+                <h3 class="title">{{item.item_label}}</h3>
+                <p class="desc">{{item.item_desc}}</p>
               </div>
             </a>
           </li>
         </ul>
       </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="cont">
-        <ul>
-          <li>
-            <a href="#">
-              <div class="cont_main">
-                <h3 class="title">云服务器</h3>
-                <p class="desc">稳定安全，高易用可弹性伸缩的计算服务</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-
     </div>
   </div>
 </template>
 
 <script>
   export default {
-      name: "IBeautifulTabLink"
+    name: "IBeautifulTabLink",
+    props: {
+      // 带有默认值的对象
+      pData: {
+        type: Object,
+        // 对象或数组默认值必须从一个工厂函数获取
+        default: function () {
+          return {
+            title: '产品定价',
+            pItemData:[
+              {
+                itemTitle:'热门',
+                items:[
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器1',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                ],
+              },
+              {
+                itemTitle:'计算',
+                items:[
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器2',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                ],
+              },
+              {
+                itemTitle:'存储',
+                items:[
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器3',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                ],
+              },
+              {
+                itemTitle:'视频服务',
+                items:[
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器4',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                ],
+              },
+              {
+                itemTitle:'数据库',
+                items:[
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                  {item_label:'云服务器5',item_desc:'稳定安全，高易用可弹性伸缩的计算服务',item_link:'https://www.baidu.com',},
+                ],
+              }
+            ]
+          }
+        }
+      },
+    },
+    methods:{
+      bindPrevNextEvent:function () {
+        //选项卡切换
+        $('.category ul li').click(function(){
+          var indexC = $(this).index();
+          $(this).addClass('active').siblings().removeClass('active');
+          $('.cont').eq(indexC).addClass('active').siblings().removeClass('active');
+        })
+      }
+    },
+    mounted(){
+      this.bindPrevNextEvent();
+    }
   }
 </script>
 
@@ -371,55 +191,6 @@
     width: 0;
     height: 100%;
     vertical-align: middle;
-  }
-
-  .price .category .prev {
-    display: none;
-    position: absolute;
-    top: 0;
-    width: 59px;
-    height: 100%;
-    box-shadow: 0 0 3px #c6c6c7;
-    background: #fff;
-    text-align: center;
-    font-size: 0;
-    left: 0;
-    border-right: 1px solid #e1e3e6;
-  }
-
-  .price .category .prev span {
-    display: inline-block;
-    vertical-align: middle;
-    width: 9px;
-    height: 14px;
-    background-repeat: no-repeat;
-    background-image: url(../../../assets/svg/arrow.svg);
-    background-position: 96.77% 25%;
-    background-size: initial;
-  }
-
-  .price .category .next span {
-    display: inline-block;
-    vertical-align: middle;
-    width: 9px;
-    height: 14px;
-    background-repeat: no-repeat;
-    background-image: url(../../../assets/svg/arrow.svg);
-    background-position: 61.29% 25%;
-    background-size: initial;
-  }
-
-  .price .category .next {
-    position: absolute;
-    top: 0;
-    width: 59px;
-    height: 100%;
-    box-shadow: 0 0 3px #c6c6c7;
-    background: #fff;
-    text-align: center;
-    font-size: 0;
-    right: 0;
-    border-left: 1px solid #e1e3e6;
   }
 
   .price .category ul li {
