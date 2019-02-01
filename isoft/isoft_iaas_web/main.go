603cd99753235d3a91041f630683997d86e9323c
389093982 <389093982@qq.com>
2019-02-01 20:49:51 +0800

    代码优化

--- isoft/isoft_iaas_web/main.go
+++ isoft/isoft_iaas_web/main.go
@@ -40,7 +40,7 @@ func init() {
 func initLog() {
 	var logDir string
 	if beego.BConfig.RunMode == "dev" || beego.BConfig.RunMode == "local" {
-		logDir = "../../../isoft_blog_web_log"
+		logDir = "../../../isoft_iaas_web_log"
 	} else {
 		// 日志文件所在目录
 		logDir = fileutil.ChangeToLinuxSeparator(apppath.GetAPPRootPath() + "/isoft_iaas_web_log")
@@ -56,7 +56,7 @@ func initLog() {
 	logs.SetLogger(logs.AdapterConsole)
 	// 多文件输出
 	logs.SetLogger(logs.AdapterMultiFile,
-		`{"filename":"`+logDir+`/isoft_blog_web.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
+		`{"filename":"`+logDir+`/isoft_iaas_web.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
 	// 输出文件名和行号
 	logs.EnableFuncCallDepth(true)
 	// 异步输出日志
