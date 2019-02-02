21b9e363b48bc0b554bc8c885872fe872bf05652
389093982 <389093982@qq.com>
2019-02-02 13:49:00 +0800

    修改搜索bug

--- isoft/isoft_iaas_web/controllers/iwork/iwork.go
+++ isoft/isoft_iaas_web/controllers/iwork/iwork.go
@@ -31,6 +31,9 @@ func (this *WorkController) FilterPageWork()  {
 	condArr := make(map[string]string)
 	offset, _ := this.GetInt("offset", 10)            // 每页记录数
 	current_page, _ := this.GetInt("current_page", 1) // 当前页
+	if search := this.GetString("search");search != "" {
+		condArr["search"] = search
+	}
 	works, count, err := iwork.QueryWork(condArr, current_page, offset)
 	paginator := pagination.SetPaginator(this.Ctx, offset, count)
 	if err == nil {
