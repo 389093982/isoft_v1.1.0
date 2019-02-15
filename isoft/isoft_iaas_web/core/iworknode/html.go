c8b765fbfe34f70a032ffdbb010da16e5f9bc6d9
389093982 <389093982@qq.com>
2019-02-15 22:19:39 +0800

    代码优化

---
+++ isoft/isoft_iaas_web/core/iworknode/html.go
@@ -0,0 +1,41 @@
+package iworknode
+
+import (
+	"isoft/isoft_iaas_web/core/iworkdata/datastore"
+	"isoft/isoft_iaas_web/core/iworkdata/schema"
+	"isoft/isoft_iaas_web/core/iworkutil/htmlutil"
+	"isoft/isoft_iaas_web/models/iwork"
+)
+
+type HrefParserNode struct {
+	BaseNode
+	WorkStep *iwork.WorkStep
+}
+
+func (this *HrefParserNode) Execute(trackingId string) {
+	// 数据中心
+	dataStore := datastore.GetDataSource(trackingId)
+	// 节点中间数据
+	tmpDataMap := this.FillParamInputSchemaDataToTmp(this.WorkStep, dataStore)
+	if url, ok := tmpDataMap["url"].(string); ok{
+		if hrefs := htmlutil.GetAllHref(url); len(hrefs) > 0{
+			dataStore.CacheData(this.WorkStep.WorkStepName, "hrefs", hrefs)
+		}
+	}
+}
+
+func (this *HrefParserNode) GetDefaultParamInputSchema() *schema.ParamInputSchema {
+	return schema.BuildParamInputSchemaWithSlice([]string{"url"})
+}
+
+func (this *HrefParserNode) GetRuntimeParamInputSchema() *schema.ParamInputSchema {
+	return &schema.ParamInputSchema{}
+}
+
+func (this *HrefParserNode) GetDefaultParamOutputSchema() *schema.ParamOutputSchema {
+	return schema.BuildParamOutputSchemaWithSlice([]string{"hrefs"})
+}
+
+func (this *HrefParserNode) GetRuntimeParamOutputSchema() *schema.ParamOutputSchema {
+	return &schema.ParamOutputSchema{}
+}
