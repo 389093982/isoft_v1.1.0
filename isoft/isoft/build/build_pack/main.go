f583b8ac8ee37db24840e4f3c88f75cd6180e252
389093982 <389093982@qq.com>
2019-02-18 14:00:57 +0800

    代码优化

--- isoft/isoft/build/build_pack/main.go
+++ isoft/isoft/build/build_pack/main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	// 设置环境变量 GOOS:目标平台的操作系统
+	// 交叉编译依赖下面几个环境变量:
+	// $GOARCH 目标平台（编译后的目标平台）的处理器架构（386、amd64、arm）
+	// $GOOS 目标平台（编译后的目标平台）的操作系统（darwin、freebsd、linux、windows）
 	GOOS := os.Getenv("GOOS")
 	os.Setenv("GOOS", "linux")
 	defer os.Setenv("GOOS", GOOS)
