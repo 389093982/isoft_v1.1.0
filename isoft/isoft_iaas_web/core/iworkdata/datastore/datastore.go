1df73d1705ea056b60de62ae1bcaf8013db3cbcf
389093982 <389093982@qq.com>
2019-03-23 20:12:07 +0800

    代码优化

--- isoft/isoft_iaas_web/core/iworkdata/datastore/datastore.go
+++ isoft/isoft_iaas_web/core/iworkdata/datastore/datastore.go
@@ -5,15 +5,13 @@ import (
 	"isoft/isoft_iaas_web/models/iwork"
 )
 
-var datastores = make(map[string]*DataStore, 0)
-
 type DataNodeStore struct {
 	NodeOutputDataMap map[string]interface{} // 当前节点的输出参数 map
 }
 
 type DataStore struct {
-	TrackingId   string
-	nodeStoreMap map[string]*DataNodeStore
+	TrackingId       string
+	DataNodeStoreMap map[string]*DataNodeStore
 }
 
 // 向数据中心缓存数据
@@ -25,29 +23,29 @@ func (this *DataStore) CacheData(nodeName, paramName string, paramValue interfac
 // 存储字节数据,不用记录日志
 func (this *DataStore) CacheByteData(nodeName, paramName string, paramValue interface{}) {
 	// 为当前 nodeName 绑定 DataNodeStore 数据空间
-	if _, ok := this.nodeStoreMap[nodeName]; !ok {
-		this.nodeStoreMap[nodeName] = &DataNodeStore{
+	if _, ok := this.DataNodeStoreMap[nodeName]; !ok {
+		this.DataNodeStoreMap[nodeName] = &DataNodeStore{
 			NodeOutputDataMap: make(map[string]interface{}, 0),
 		}
 	}
 	// 存数据
-	dataNodeStore := this.nodeStoreMap[nodeName]
+	dataNodeStore := this.DataNodeStoreMap[nodeName]
 	dataNodeStore.NodeOutputDataMap[paramName] = paramValue
 }
 
 // 从数据中心获取数据
 func (this *DataStore) GetData(nodeName, paramName string) interface{} {
-	store := this.nodeStoreMap[nodeName]
+	store := this.DataNodeStoreMap[nodeName]
 	if store == nil {
 		return nil
 	}
-	return this.nodeStoreMap[nodeName].NodeOutputDataMap[paramName]
+	return this.DataNodeStoreMap[nodeName].NodeOutputDataMap[paramName]
 }
 
 // 获取数据中心
 func InitDataStore(trackingId string) *DataStore {
 	return &DataStore{
-		TrackingId:   trackingId,
-		nodeStoreMap: make(map[string]*DataNodeStore, 0),
+		TrackingId:       trackingId,
+		DataNodeStoreMap: make(map[string]*DataNodeStore, 0),
 	}
 }
