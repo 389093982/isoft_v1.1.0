193de87e28e52c1dd2db37983749cb14fe13ac6c
389093982 <389093982@qq.com>
2019-04-02 23:40:02 +0800

    代码优化

--- isoft/isoft_iaas_web/core/iworkutil/migrateutil/migrateutil.go
+++ isoft/isoft_iaas_web/core/iworkutil/migrateutil/migrateutil.go
@@ -18,97 +18,100 @@ type MigrateExecutor struct {
 	TrackingId string
 }
 
-func checkError(err error, detail ...string) {
-	if err != nil {
-		if len(detail) > 0 && detail[0] != "" {
-			panic(errors.New(fmt.Sprintf("%s : %s", detail[0], err.Error())))
-		} else {
-			panic(err)
-		}
-	}
-}
-
-func (this *MigrateExecutor) ping() {
+func (this *MigrateExecutor) ping() (err error) {
 	if this.Dsn == "" {
-		panic(errors.New("empty dsn error..."))
+		return errors.New("empty dsn error...")
 	}
 	// 建立连接
-	db, err := sql.Open("mysql", this.Dsn)
-	checkError(err)
-	this.db = db
+	this.db, err = sql.Open("mysql", this.Dsn)
+	return nil
 }
 
 // 建立迁移文件版本管理表
-func (this *MigrateExecutor) initial() {
+func (this *MigrateExecutor) initial() (err error) {
 	versionTable := `CREATE TABLE IF NOT EXISTS migrate_version 
 		(id INT(20) PRIMARY KEY AUTO_INCREMENT, tracking_id CHAR(200), flag CHAR(200), hash CHAR(200),sql_detail TEXT, tracking_detail TEXT, created_time datetime);`
-	this.ExecSQL(versionTable)
-}
-
-func (this *MigrateExecutor) ExecSQLWithLogger(detail, sql string, args ...interface{}) (rs sql.Result, err error) {
-	if detail != "" {
-		detail = fmt.Sprintf("[id = %s][sql = %s]", detail, sql)
-	}
-	stmt, err := this.db.Prepare(sql)
-	checkError(err, detail)
-	rs, err = stmt.Exec(args...)
-	checkError(err, detail)
+	_, err = this.ExecSQL(versionTable)
 	return
 }
 
 func (this *MigrateExecutor) ExecSQL(sql string, args ...interface{}) (rs sql.Result, err error) {
-	return this.ExecSQLWithLogger("", sql, args...)
-}
-
-func (this *MigrateExecutor) QueryRowSQL(sql string, args ...interface{}) (row *sql.Row) {
 	stmt, err := this.db.Prepare(sql)
-	checkError(err)
-	row = stmt.QueryRow(args...)
+	if err != nil {
+		return nil, err
+	}
+	rs, err = stmt.Exec(args...)
 	return
 }
 
-func (this *MigrateExecutor) record(flag, hash, sql, tracking_detail string) {
-	if this.db != nil {
-		recordLog := `INSERT INTO migrate_version(tracking_id,flag,hash,sql_detail,tracking_detail, created_time) VALUES (?,?,?,?,?,NOW());`
-		this.ExecSQL(recordLog, this.TrackingId, flag, hash, sql, tracking_detail)
+func (this *MigrateExecutor) QueryRowSQL(sql string, args ...interface{}) (row *sql.Row, err error) {
+	if stmt, err := this.db.Prepare(sql); err == nil {
+		row = stmt.QueryRow(args...)
 	}
+	return
 }
 
-func (this *MigrateExecutor) migrate() {
-	migrates, err := iwork.QueryAllMigrate()
-	checkError(err)
-	for _, migrate := range migrates {
-		this.migrateOne(migrate)
+func (this *MigrateExecutor) record(flag, hash, sql, tracking_detail string) error {
+	if this.db != nil {
+		recordLog := `INSERT INTO migrate_version(tracking_id,flag,hash,sql_detail,tracking_detail, created_time) VALUES (?,?,?,?,?,NOW());`
+		_, err := this.ExecSQL(recordLog, this.TrackingId, flag, hash, sql, tracking_detail)
+		return err
 	}
+	return nil
+}
+
+func (this *MigrateExecutor) migrate() error {
+	migrates, err := iwork.QueryAllMigrate()
+	if err != nil {
+		return err
+	}
+	for _, migrate := range migrates {
+		if err := this.migrateOne(migrate); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (this *MigrateExecutor) checkExecuted(hash string) bool {
 	sql := `SELECT COUNT(*) FROM migrate_version WHERE hash = ?`
-	row := this.QueryRowSQL(sql, hash)
-	var datacount int64
-	if err := row.Scan(&datacount); err == nil && datacount > 0 {
-		return true
+	if row, err := this.QueryRowSQL(sql, hash); err == nil {
+		var datacount int64
+		if err := row.Scan(&datacount); err == nil && datacount > 0 {
+			return true
+		}
 	}
 	return false
 }
 
-func (this *MigrateExecutor) migrateOne(migrate iwork.TableMigrate) {
+func (this *MigrateExecutor) migrateOne(migrate iwork.TableMigrate) error {
 	hash := hashutil.CalculateHashWithString(migrate.TableMigrateSql)
 	if !this.checkExecuted(hash) {
 		// 每次迁移都有可能有多个执行 sql
 		executeSqls := strings.Split(migrate.TableMigrateSql, ";")
+		tx, err := this.db.Begin()
+		if err != nil {
+			return err
+		}
 		for _, executeSql := range executeSqls {
 			if strings.TrimSpace(executeSql) != "" {
 				detailHash := hashutil.CalculateHashWithString(executeSql)
 				if !this.checkExecuted(detailHash) {
-					this.ExecSQLWithLogger(strconv.FormatInt(migrate.Id, 10), executeSql)
-					this.record("true", detailHash, executeSql, "")
+					if _, err := this.ExecSQL(executeSql); err == nil {
+						this.record("true", detailHash, executeSql, "")
+					} else {
+						tx.Rollback()
+						errorMsg := fmt.Sprintf("[%s] - [%s] : %s", strconv.FormatInt(migrate.Id, 10), executeSql, err.Error())
+						return errors.New(errorMsg)
+					}
 				}
 			}
 		}
+		tx.Commit()
 		// 计算hash 值
 		this.record("true", hash, migrate.TableMigrateSql, "")
 	}
+	return nil
 }
 
 func MigrateToDB(dsn string) (err error) {
@@ -116,15 +119,13 @@ func MigrateToDB(dsn string) (err error) {
 		Dsn:        dsn,
 		TrackingId: stringutil.RandomUUID(),
 	}
-	defer func() {
-		if err1 := recover(); err1 != nil {
-			err = err1.(error)
-			executor.record("false", "", "", err.Error())
+	if err = executor.ping(); err == nil {
+		if err = executor.initial(); err == nil {
+			err = executor.migrate()
 		}
-	}()
-
-	executor.ping()
-	executor.initial()
-	executor.migrate()
-	return nil
+	}
+	if err != nil {
+		executor.record("false", "", "", err.Error())
+	}
+	return
 }
