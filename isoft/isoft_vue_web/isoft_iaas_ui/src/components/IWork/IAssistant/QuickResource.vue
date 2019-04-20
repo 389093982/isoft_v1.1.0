ad881b83172babc6baac40658e9adae1898e98a2
389093982 <389093982@qq.com>
2019-04-20 22:17:38 +0800

    代码优化

--- isoft/isoft_vue_web/isoft_iaas_ui/src/components/IWork/IAssistant/QuickResource.vue
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/IWork/IAssistant/QuickResource.vue
@@ -1,39 +1,19 @@
 <template>
   <div style="margin: 10px;">
     <span v-for="tableName in tableNames">
-      <p style="color: red;">表名：{{tableName}}</p>
-      <Row>
-        <Col span="4">
-          <span v-for="(tableColumns, _tableName) in tableColumnsMap">
-            <span v-if="_tableName == tableName">
-              <CheckboxGroup>
-                <ul>
-                  <li v-for="tableColumn in tableColumns" style="list-style: none;">
-                    <Checkbox :label="tableColumn"></Checkbox>
-                  </li>
-                </ul>
-              </CheckboxGroup>
-            </span>
-          </span>
-        </Col>
-
-        <Col span="20">
-          <span v-for="(tableSqls, _tableName) in tableSqlMap">
-            <span v-if="_tableName == tableName">
-              <p v-for="tableSql in tableSqls">{{tableSql}}</p>
-            </span>
-          </span>
-        </Col>
-      </Row>
+      <QuickTable :table-name="tableName" :table-columns="getTableColumns(tableName)"
+        :table-sqls="getTableSqls(tableName)"/>
     </span>
   </div>
 </template>
 
 <script>
   import {LoadQuickSqlMeta} from "../../../api"
+  import QuickTable from "./QuickTable"
 
   export default {
     name: "QuickResource",
+    components:{QuickTable},
     props:{
       resource:{
         type:Object,
@@ -55,6 +35,12 @@
           this.tableColumnsMap = result.tableColumnsMap;
           this.tableSqlMap = result.tableSqlMap;
         }
+      },
+      getTableColumns:function (tableName) {
+        return this.tableColumnsMap[tableName];
+      },
+      getTableSqls:function (tableName) {
+        return this.tableSqlMap[tableName];
       },
     },
     mounted(){
---
+++ isoft/isoft_vue_web/isoft_iaas_ui/src/components/IWork/IAssistant/QuickTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <div style="margin: 20px;">
+    <Row>
+      <Col span="4">
+        <p style="color: red;">表名：{{tableName}}</p>
+        <CheckboxGroup>
+          <ul>
+            <li v-for="tableColumn in tableColumns" style="list-style: none;">
+              <Checkbox :label="tableColumn"></Checkbox>
+            </li>
+          </ul>
+        </CheckboxGroup>
+      </Col>
+      <Col span="20">
+        <p style="color: red;">sql信息</p>
+        <span v-for="tableSql in tableSqls">
+          <p>{{tableSql}}</p>
+        </span>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "QuickTable",
+    props:{
+      tableName:{
+        type:String,
+        default:'',
+      },
+      tableColumns:{
+        type:Array,
+        default:[],
+      },
+      tableSqls:{
+        type:Array,
+        default:[],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
